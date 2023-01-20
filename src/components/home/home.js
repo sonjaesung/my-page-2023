9f2534edcd1983259166ec27544094217e0d936e
lokises <lokises@mcircle.biz>
2023-01-21 00:51:32 +0900

    메인 슬라이드

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import { observer } from 'mobx-react';
+import MainBanner from './mainBanner';
 
 const HomeMain = observer(() => {
 
     return (
         <div className='homeMain'>
-
+            <MainBanner />
 
         </div>
     );
--- src/components/home/mainBanner.js
+++ src/components/home/mainBanner.js
@@ -1,12 +1,33 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React from 'react';
 import { observer } from 'mobx-react';
+import SimpleImageSlider from 'react-simple-image-slider';
 
 const MainBanner = observer(() => {
 
+    const images = [
+        { url: "img/HTML5-logo.png" },
+        { url: "img/CSS3-logo.png" },
+        { url: "img/JavaScript-logo.png" },
+        { url: "img/React-logo.png" },
+        { url: "img/Redux-logo.png" },
+        { url: "img/Mobex-logo.png" },
+        { url: "img/Nodejs-logo.png" },
+        { url: "img/MySql-logo.png" },
+    ];
+
     return (
-        <div className='homeMain'>
-
-
+        <div className='mVisual'>
+            <div className='bx-wrapper'>
+                <div className='bx-viewport'>
+                    <SimpleImageSlider
+                        width={'100%'}
+                        height={'812px'}
+                        images={images}
+                        showBullets={true}
+                        showNavs={true}
+                    />
+                </div>
+            </div>
         </div>
     );
 });
