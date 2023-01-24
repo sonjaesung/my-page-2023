4b8b1837a5db8d41f544c45d9f9ac914f8f6d8a6
lokises <lokises@mcircle.biz>
2023-01-24 22:33:29 +0900

    제안하기

--- src/components/common/QuickBottom.js
+++ src/components/common/QuickBottom.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
 
 const QuickBottom = observer(() => {
     return (
         <div className="quickBtm">
             <div className='inConts'>
                 <strong className='recommand'>이직 제안 문의</strong>
-                <a href='/' className='quickBtn'>제안하기</a>
+                <Link to='/developer/basic' className='quickBtn'>
+                    제안하기
+                </Link>
             </div>
         </div>
     );
